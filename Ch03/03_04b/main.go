9dcdc32c9b48a17b4413edf0814a02d27c5fed7d
Sai Indra <98470966+SaiIndra2003@users.noreply.github.com>
2025-02-20 18:38:22 +0000

    slices

--- Ch03/03_04b/main.go
+++ Ch03/03_04b/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	// This is an array
-	var colors = [3]string{"Red", "Green", "Blue"}
+	// This is an slice
+	var colors = []string{"Red", "Green", "Blue"}
+	newColors := remove(append(colors, "Black","Pinks"),4)
+	sort.Strings(newColors)
 	fmt.Println(colors)
+	fmt.Println(newColors)
+}
+
+func remove(slice []string, i int)[] string{
+	return append(slice[:i],slice[i+1:]...)
 }
