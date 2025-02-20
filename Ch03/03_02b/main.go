b59027b0a87c26149e771a313ddf3657af2a8a98
Sai Indra <98470966+SaiIndra2003@users.noreply.github.com>
2025-02-20 18:19:27 +0000

    Working with pointers

--- Ch03/03_02b/main.go
+++ Ch03/03_02b/main.go
@@ -6,4 +6,16 @@ import (
 
 func main() {
 	fmt.Println("Pointers")
+	var p *int
+	anInt := 42
+	p = &anInt
+	if p != nil{
+		fmt.Println("P is refering to :", *p," P is at ",p)
+	} else {
+		fmt.Println("P is Nil")
+	}
+
+	value1:= 42.3
+	pointer1 :=&value1
+	fmt.Println("Value 1:",*pointer1, "Located at :",pointer1)
 }
