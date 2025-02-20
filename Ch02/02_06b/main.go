ee19f643dd624110aed9490708c128404dcdd228
Sai Indra <98470966+SaiIndra2003@users.noreply.github.com>
2025-02-20 16:07:30 +0000

    time and date

--- Ch02/02_06b/main.go
+++ Ch02/02_06b/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
+	t := time.Date(2003, time.May, 18,23, 0, 0, 0, time.UTC)
+//	t = time.Now()
+	fmt.Printf("I was born at %s\n", t)
+
+	now := time.Now()
+	fmt.Printf("Current time %s\n", now)
 	fmt.Println("Dates and times")
+
+	fmt.Printf(now.Format(time.ANSIC) + "\n")
 
 }
