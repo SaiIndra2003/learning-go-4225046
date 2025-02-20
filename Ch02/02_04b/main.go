c8e0672f781148de5661035c264e7696bbe59da3
Sai Indra <98470966+SaiIndra2003@users.noreply.github.com>
2025-02-20 15:31:59 +0000

    Math operations

--- Ch02/02_04b/main.go
+++ Ch02/02_04b/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Math")
+	i1, i2, i3 := 12, 45, 68
+	intSum := i1 + i2 + i3
+
+
+
+	fmt.Println("Total Sum is :", intSum)
+
+	f1, f2, f3:= 23.5,63.1,77.3
+
+	floatSum := f1 + f2 + f3
+
+	fmt.Println("Total Sum is :", floatSum)	
+
+	total := float64(i1) + f1
+	
+	fmt.Println("Total Sum is :", total)
 }
