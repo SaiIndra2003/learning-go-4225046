17667636f40b3415c328e1e290da62c1fee4dd00
Sai Indra <98470966+SaiIndra2003@users.noreply.github.com>
2025-02-20 14:26:42 +0000

    fmt package

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,16 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNum := 42
+	fmt.Println(str1,str2,str3)
+	strLen, err := fmt.Println("The Number is :",aNum)
+	if err == nil{
+		fmt.Println("String Length is :",strLen)
+	}
 
-	fmt.Println("Hello from Go!")
+	fmt.Printf("Type of number : %T\n", aNum)
+
 }
