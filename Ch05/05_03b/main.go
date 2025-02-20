c4dd62404a4c814d28bc8d4c426af84af92970c2
Sai Indra <98470966+SaiIndra2003@users.noreply.github.com>
2025-02-20 19:39:08 +0000

    goroutines

--- Ch05/05_03b/main.go
+++ Ch05/05_03b/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Goroutines")
+	go say("Hello from thread")
+	fmt.Println("Hello from main")
+	time.Sleep(2 * time.Second)
+	
+}
+
+func say(message string){
+	time.Sleep(1 * time.Second)
+	fmt.Println(message)
 }
