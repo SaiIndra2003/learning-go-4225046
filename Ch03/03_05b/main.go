07c40373ebcb7ef20c42724376c7b04cf8310bb9
Sai Indra <98470966+SaiIndra2003@users.noreply.github.com>
2025-02-20 18:46:09 +0000

    maps

--- Ch03/03_05b/main.go
+++ Ch03/03_05b/main.go
@@ -5,5 +5,13 @@ import (
 )
 
 func main() {
-	fmt.Println("Maps")
+	states := make(map[string]string)
+	states["Ind"] = "India"
+	states["Ch"] = "China"
+	fmt.Println(states)
+	for k,v := range states{
+		fmt.Printf("%v : %v \n",k,v)
+	}
+	delete(states,"Ch")
+	fmt.Println(states)
 }
