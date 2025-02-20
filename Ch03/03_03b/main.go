1ca929d9316ad01554635ead65c8c86d5d1509f8
Sai Indra <98470966+SaiIndra2003@users.noreply.github.com>
2025-02-20 18:28:34 +0000

    Arrays

--- Ch03/03_03b/main.go
+++ Ch03/03_03b/main.go
@@ -6,4 +6,12 @@ import (
 
 func main() {
 	fmt.Println("Arrays")
+	var colors [3]string 
+	colors[0] = "Blue"
+	colors[1] = "Pink"
+	colors[2] = "Green"
+	fmt.Println(colors)
+
+	nums := [5]int{0,1,2,3,4}
+	fmt.Println(nums)
 }
