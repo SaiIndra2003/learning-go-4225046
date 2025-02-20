b2e62bff1f9c9c8c358200611ac575c20a2fa5e1
Sai Indra <98470966+SaiIndra2003@users.noreply.github.com>
2025-02-20 15:35:29 +0000

    math package

--- Ch02/02_05b/main.go
+++ Ch02/02_05b/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	sum := f1 + f2 + f3
+	sum = math.Round(sum*100) / 100
 	fmt.Println("Float sum:", sum)
 
 }
